d2d791add1d0cb6f10979ef1f15ed1b25c487fef
George Borsato <georginhoborsato@gmail.com>
2022-01-17 09:49:26 -0300

    feat: first variable

---
+++ variables/variable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//explicito a tipagem
+	var var1 string = "Teste"
+
+	//oculto a tipagem
+	var2 := "teste2"
+
+	fmt.Println(var1)
+	fmt.Println(var2)
+}

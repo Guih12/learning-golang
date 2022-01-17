35906dbecca404775e9716898e0da2b3365ae77e
George Borsato <georginhoborsato@gmail.com>
2022-01-17 17:43:20 -0300

    feat: switch case

--- conditions/condition.go
+++ conditions/condition.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+func dayOfWeek(number int) string {
+	switch number {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda"
+	case 3:
+		return "Terça"
+	default:
+		return "Valor invalido"
+	}
+}
+
+//outra forma de escrever switch com go
+func dayOfWeekTwo(number int) string {
+	switch {
+	case number == 1:
+		return "Domingo"
+	default:
+		return "Valor invalido"
+	}
+}
+
 func main() {
 
 	number := 10
@@ -15,4 +38,10 @@ func main() {
 	if otherNumber := number; otherNumber > 0 {
 		fmt.Println("numero é maior que zero")
 	}
+
+	dia := dayOfWeek(1)
+	fmt.Println(dia)
+
+	dia2 := dayOfWeekTwo(1)
+	fmt.Println(dia2)
 }

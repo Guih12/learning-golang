33db1d4072a15051c9ebd29c72da5a66f98b0ae0
George Borsato <georginhoborsato@gmail.com>
2022-01-20 11:13:12 -0300

    feat: sync goroutines

---
+++ wait_group/wait.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func writeDisplay(param string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(param)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	//concorrência != paralelismo
+	var waitGroup sync.WaitGroup
+
+	waitGroup.Add(2) //grupo de espera e tem duas go routines
+
+	go func() {
+		writeDisplay("Excutando go goutine 1")
+		waitGroup.Done() //ele tira um do contador de go routines
+	}()
+
+	go func() {
+		writeDisplay("Excutando go goutine 2")
+		waitGroup.Done()
+	}()
+
+	go func() {
+		writeDisplay("Excutando go goutine 3")
+		waitGroup.Done()
+	}()
+
+	go func() {
+		writeDisplay("Excutando go goutine 4")
+		waitGroup.Done()
+	}()
+
+	waitGroup.Wait() //fala pra esperar a quantidade ser == 0 para terminar
+}
